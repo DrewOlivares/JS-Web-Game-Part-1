4acc025bb4ec9c2e17399d6d40425a68c7577281
Andrew Olivares <Wrestler533438@gmail.com>
2022-12-13 17:22:59 -0800

    finished with activity

--- index.js
+++ index.js
@@ -6,12 +6,45 @@ function newImage(a, b, c, d,) {
     image.style.bottom = c
     document.body.append (image)
 }
+function newItem(a, b, c){
+    let item = document.createElement('img')
+item.src = a
+item.position = 'fixed'
+item.left = b
+item.bottom = c
+document.body.append(item)
+
+item.addEventListener('dblclick', function(){
+    item.remove()
+})
+}
+
+newImage('assets/green-character.gif', '100px', '100px')
+newImage('assets/pine-tree.png', '450px', '200px')
+newImage('assets/tree.png', '200px', '300px')
+newImage('assets/pillar.png', '350px', '100px')
+newImage('assets/crate.png', '150px', '200px')
+newImage('assets/well.png', '500px', '425px')
+
+newItem('assets/sword.png', '500px', '405px')
+newItem('assets/shield.png', '165px', '185px')
+newItem('assets/staff.png', '600px', '100px')
+
+
+/* let sword = document.createElement('img')
+sword.src = 'assets/sword.png'
+sword.position = 'fixed'
+sword.left = '500px'
+sword.bottom = '405px'
+document.body.append(sword)
+
+sword.addEventListener('click', function(){
+    sword.remove()
+})
+ */
 /* let greenCharacter = document.createElement('img')
 greenCharacter.src = 'assets/green-character.gif'
 greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
 document.body.append(greenCharacter) */
-
-newImage('assets/green-character.gif', '100px', '100px')
-newImage('assets/pine-tree.png', '450px', '200px')
